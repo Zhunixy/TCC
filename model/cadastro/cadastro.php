2e30fe2206e5c427e6a84bf6b4739f4d32df3bb6
Zhunixy <kauan8643@gmail.com>
2024-02-28 14:51:00 -0300

    Back-end Cadastro fim

--- model/cadastro/cadastro.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$usuario = $_POST['nome_usuario'];
-$email = $_POST['email_usuario'];
-$senha = $_POST['senha_usuario'];
-
--- model/cadastro/conexao.php
+++ model/cadastro/conexao.php
@@ -1,11 +1,11 @@
 <?php
 
 $usuario = 'root';
-$senha = '';
-$database = 'tcc';
+$psenha = '';
+$database = 'TCC';
 $host = 'localhost';
 
-$conexao = new mysqli($usuario,$senha,$database,$host);
+$conexao = new mysqli($user,$password,$database,$host);
 
 if($conexao -> connect_error){
     die("Falha ao conectar ao banco de dados" . $conexao -> connect_error);
